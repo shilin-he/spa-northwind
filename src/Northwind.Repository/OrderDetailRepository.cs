86e8cc96de11d4a1a6a78aa7057b2adc9a439100
Shilin He <shilin.he@gmail.com>
2013-11-27 20:22:53 -0700

    Add OrderDetail.

---
+++ src/Northwind.Repository/OrderDetailRepository.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using Northwind.Model;
+
+namespace Northwind.Repository
+{
+    public interface IOrderDetailRepository : IDisposable
+    {
+        OrderDetail GetOrderDetail(int orderId, int productId);
+        IEnumerable<OrderDetail> GetOrderDetails(int orderId);
+        OrderDetail AddOrderDetail(OrderDetail orderDetai);
+        OrderDetail UpdateOrderDetail(OrderDetail orderDetail);
+        void DeleteOrderDetail(int orderId);
+    }
+
+    public class OrderDetailRepository : IOrderDetailRepository
+    {
+        private readonly NorthwindContext _ctx;
+
+        public OrderDetailRepository()
+        {
+            _ctx = new NorthwindContext();
+        }
+
+        public OrderDetail GetOrderDetail(int orderId, int productId)
+        {
+            return _ctx.OrderDetails.Find(orderId, productId);
+        }
+
+        public IEnumerable<OrderDetail> GetOrderDetails(int orderId)
+        {
+            return _ctx.OrderDetails.Where(od => od.OrderId == orderId);
+        }
+
+        public OrderDetail AddOrderDetail(OrderDetail orderDetai)
+        {
+            _ctx.OrderDetails.Add(orderDetai);
+            _ctx.SaveChanges();
+            return orderDetai;
+        }
+
+        public OrderDetail UpdateOrderDetail(OrderDetail orderDetail)
+        {
+            _ctx.Entry(orderDetail).State = EntityState.Modified;
+            _ctx.SaveChanges();
+            return orderDetail;
+        }
+
+        public void DeleteOrderDetail(int orderId)
+        {
+            _ctx.OrderDetails.Remove(_ctx.OrderDetails.FirstOrDefault(od => od.OrderId == orderId));
+            _ctx.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            _ctx.Dispose();
+        }
+    }
+}
---
+++ src/Northwind.UI/Models/OrderDetailDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Northwind.UI.Models
+{
+    public class OrderDetailDto
+    {
+        public int OrderId { get; set; }
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public decimal UnitPrice { get; set; }
+        public short Quantity { get; set; }
+        public float Discount { get; set; }
+    }
+}
